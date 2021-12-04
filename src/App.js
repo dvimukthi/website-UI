28fcd927e6bbae3fa74064f162d76f70629631ec
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-04 20:52:13 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { css } from "@emotion/core";
 import PropagateLoader from "react-spinners/PropagateLoader";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const override = css`
+    display: block;
+    border-color: red;
+    margin-top: 20%;
+  `;
+
   return <div className="App">Cake</div>;
 }
 
