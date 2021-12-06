a7b99dc3eb75d062ce06d7b0bc9835398e4b5aa1
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-06 20:08:11 +0530

    nav, setnav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Navbar() {
+  const [nav, setnav] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 50) {
+      setnav(true);
+    } else {
+      setnav(false);
+    }
+  };
+
   return <div></div>;
 }
 
