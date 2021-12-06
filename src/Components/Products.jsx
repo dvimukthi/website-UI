a04150032566954dd94a0919385af5a3b7825a48
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-06 22:04:21 +0530

    Products

---
+++ src/Components/Products.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Productbox from "./Productbox";
+
+function Products() {
+  return (
+    <div id="products">
+      <h1>Choose & Enjoy</h1>
+    </div>
+  );
+}
+
+export default Products;
