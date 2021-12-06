b834a4ce80fc1c0ae541043d0145fc865f3f2b36
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-06 22:18:06 +0530

    props

--- src/Components/Productbox.jsx
+++ src/Components/Productbox.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-function Productbox() {
+function Productbox(props) {
   return (
-    <div className='a-box'>
-        <div className='a-b-img'>
-            <img src={} alt=''/>
-        </div>
+    <div className="a-box">
+      <div className="a-b-img">
+        <img src={props.image} alt="" />
+      </div>
 
-        <div className='a-b-text'>
-            <h2> {} </h2>
-            <button className='productbox-button'>Order Now</button>
-        </div>
+      <div className="a-b-text">
+        <h2> {props.title} </h2>
+        <button className="productbox-button">Order Now</button>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Productbox;
