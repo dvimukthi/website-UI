5664cd49211a14f3ce7e84dd09e9510699207e95
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-06 22:17:25 +0530

    button

--- src/Components/Productbox.jsx
+++ src/Components/Productbox.jsx
@@ -6,6 +6,11 @@ function Productbox() {
         <div className='a-b-img'>
             <img src={} alt=''/>
         </div>
+
+        <div className='a-b-text'>
+            <h2> {} </h2>
+            <button className='productbox-button'>Order Now</button>
+        </div>
     </div>
   )
 }
