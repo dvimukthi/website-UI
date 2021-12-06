22207a7b2d32f5d37e9f5112c5b43172dc1a1547
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-06 22:29:42 +0530

    About.jsx

---
+++ src/Components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+  return (
+    <div id="about">
+      <div className="about-text">
+        <h1>Upcoming Event</h1>
+        <p>Lorenwiefiwoehfiwehfiuwehfiewfiuewfuigewiufgewifweifgiewgfiwegfi</p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
