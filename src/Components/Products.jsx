9a7bb311239291e7544322be720cc1ba8520542a
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-07 10:14:12 +0530

    add products

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Productbox from "./Productbox";
 import pimage1 from "../images/s1.png";
 import pimage2 from "../images/s2.png";
+import pimage3 from "../images/s3.png";
 
 function Products() {
   return (
@@ -11,9 +12,9 @@ function Products() {
         Lorem Products Products Products Products Products Products Products
       </p>
       <div className="a-container">
-        <Productbox image={pimage1} title="Choco Cake" />
-        <Productbox image={pimage2} title="Pie Cake" />
-        <Productbox image={pimage1} title="Choco Cake" />
+        <Productbox image={pimage1} title="Strawberry Cake" />
+        <Productbox image={pimage2} title="Chocolate Cake" />
+        <Productbox image={pimage3} title="Vanilla Cake" />
       </div>
     </div>
   );
